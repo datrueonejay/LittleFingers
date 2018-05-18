25ff6b773951bfca9e4c8daa8b5504540f9f88de
Jayden Arquelada <jayarquelada@gmail.com>
2018-05-18 02:13:15 -0400

    [WIP] Floating button moves to bottom when pressed, speech bubble menu

--- app/src/main/java/com/datrueonejay/littlefingers/Services/MainMenuService.java
+++ app/src/main/java/com/datrueonejay/littlefingers/Services/MainMenuService.java
@@ -1,21 +1,27 @@
 package com.datrueonejay.littlefingers.Services;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.PixelFormat;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 
 import com.datrueonejay.littlefingers.Models.*;
 import com.datrueonejay.littlefingers.R;
@@ -24,19 +30,21 @@ public class MainMenuService extends Service {
 
     // region Properties
     private int startId = -1;
+
+    private DisplayMetrics metrics;
     private int deviceWidth;
+    private int currHeight;
     private int overlayType;
 
     private LayoutInflater inflater;
+    private WindowManager windowManager;
 
     private FloatingAppButtonModel floatingAppButtonModel;
     private OptionsMenuModel optionsMenuModel;
     private LockedScreenModel lockedScreenModel;
 
-
     // in milliseconds
-    private int timeToHoldButton = 2000;
-    private WindowManager windowManager;
+    private final int timeToHoldButton = 2000;
     //endregion
 
     @Override
@@ -48,11 +56,18 @@ public class MainMenuService extends Service {
            this.inflater = LayoutInflater.from(this.getApplication());
            this.windowManager = (WindowManager) this.getApplication().getSystemService(Context.WINDOW_SERVICE);
 
-           DisplayMetrics metrics = new DisplayMetrics();
+           Display d = windowManager.getDefaultDisplay();
+           Point size = new Point();
+           d.getSize(size);
+           int width = size.x;
+
+           this.metrics = new DisplayMetrics();
            windowManager.getDefaultDisplay().getMetrics(metrics);
+           int a = getResources().getConfiguration().orientation;
            this.deviceWidth = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT
                    ? metrics.widthPixels
                    : metrics.heightPixels;
+           this.currHeight = metrics.heightPixels;
            // need to check based on api version with overlay type is
            this.overlayType = Build.VERSION.SDK_INT <= Build.VERSION_CODES.N_MR1
                    ? WindowManager.LayoutParams.TYPE_PHONE
@@ -76,7 +91,6 @@ public class MainMenuService extends Service {
          return null;
     }
 
-
     //region Private Methods
     private void initFloatingAppButton()
     {
@@ -89,11 +103,10 @@ public class MainMenuService extends Service {
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT
         );
+        //floatingAppButtonModel.floatingAppButtonParams.windowAnimations = android.R.style.Animation_Translucent;
         floatingAppButtonModel.floatingAppButtonParams.gravity = Gravity.CENTER_HORIZONTAL;
         floatingAppButtonModel.floatingAppButton = inflater.inflate(floatingAppButtonModel.floatingAppButtonLayoutAsInt, null);
-
-
-}
+    }
 
     private void setUpFloatingAppButton()
     {
@@ -110,11 +123,9 @@ public class MainMenuService extends Service {
 
                 case MotionEvent.ACTION_MOVE:
                 {
-
                     // see how far input moved
                     float movedY = event.getRawY() + floatingAppButtonModel.currY;
                     float movedX = event.getRawX() + floatingAppButtonModel.currX;
-
                     // ensure the window moves
                     floatingAppButtonModel.floatingAppButtonParams.y = (int)movedY;
                     floatingAppButtonModel.floatingAppButtonParams.x = (int)movedX;
@@ -129,11 +140,59 @@ public class MainMenuService extends Service {
                     if (floatingAppButtonModel.lastAction == MotionEvent.ACTION_DOWN)
                     {
                         view.performClick();
-                        this.removeFloatingAppButton();
-                        this.displayOptionsMenu();
+                        ValueAnimator translationDown = ValueAnimator.ofFloat(floatingAppButtonModel.floatingAppButtonParams.y, currHeight/2 - floatingAppButtonModel.floatingAppButton.getHeight()/2);
+                        ValueAnimator translationCenter = ValueAnimator.ofFloat(floatingAppButtonModel.floatingAppButtonParams.x, 0);
+                        translationCenter.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                floatingAppButtonModel.floatingAppButtonParams.x = (int) Math.round((float)translationCenter.getAnimatedValue());
+                                windowManager.updateViewLayout(floatingAppButtonModel.floatingAppButton, floatingAppButtonModel.floatingAppButtonParams);
+                            }
+                        });
+                        translationDown.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                floatingAppButtonModel.floatingAppButtonParams.y = (int) Math.round((float)translationDown.getAnimatedValue());
+                                windowManager.updateViewLayout(floatingAppButtonModel.floatingAppButton, floatingAppButtonModel.floatingAppButtonParams);
+                            }
+                        });
+
+                        translationDown.addListener(new Animator.AnimatorListener() {
+                            @Override
+                            public void onAnimationStart(Animator animation) {
+
+                            }
+
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                displayOptionsMenu();
+
+                            }
+
+                            @Override
+                            public void onAnimationCancel(Animator animation) {
+
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animator animation) {
+
+                            }
+                        });
+                        translationDown.setDuration(500);
+                        translationCenter.setDuration(500);
+                        translationDown.start();
+                        translationCenter.start();
+
+                        //floatingAppButtonModel.floatingAppButtonParams.y = currHeight/2 - floatingAppButtonModel.floatingAppButton.getHeight()/2;
+                        //floatingAppButtonModel.floatingAppButtonParams.x = 0;
+//                        floatingAppButtonModel.currY = currHeight/2 - floatingAppButtonModel.floatingAppButton.getHeight()/2;
+                        windowManager.updateViewLayout(floatingAppButtonModel.floatingAppButton, floatingAppButtonModel.floatingAppButtonParams);
+
+//                        this.removeFloatingAppButton();
+//                        this.displayOptionsMenu();
                     }
                 }
-
                 default:
                     return false;
             }
@@ -170,7 +229,6 @@ public class MainMenuService extends Service {
         {
             removeOptionsMenu();
             displayLockedScreen();
-
         });
 
         optionsMenuModel.optionsMenu.findViewById(R.id.options).findViewById(R.id.closeAppButton).setOnClickListener(v ->
@@ -183,6 +241,11 @@ public class MainMenuService extends Service {
     private void displayOptionsMenu()
     {
         this.windowManager.addView(optionsMenuModel.optionsMenu, optionsMenuModel.optionsMenuParams);
+    }
+
+    private void removeOptionsMenu()
+    {
+        this.windowManager.removeViewImmediate(optionsMenuModel.optionsMenu);
     }
 
     private void initLockedScreen()
@@ -200,7 +263,6 @@ public class MainMenuService extends Service {
         this.lockedScreenModel.lockedScreenButtonTwo = lockedScreenModel.lockedScreen.findViewById(R.id.buttonTwo);
         this.lockedScreenModel.lockedScreenButtonThree = lockedScreenModel.lockedScreen.findViewById(R.id.buttonThree);
         this.lockedScreenModel.lockedScreenButtonFour = lockedScreenModel.lockedScreen.findViewById(R.id.buttonFour);
-
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -209,22 +271,22 @@ public class MainMenuService extends Service {
         Handler handler = new Handler();
         Runnable run1 = () -> {
             lockedScreenModel.isButtonOneHeld = true;
-            stopLockedScreen();
+            removeLockedScreen();
         };
 
         Runnable run2 = () -> {
             lockedScreenModel.isButtonTwoHeld = true;
-            stopLockedScreen();
+            removeLockedScreen();
         };
 
         Runnable run3 = () -> {
             lockedScreenModel.isButtonThreeHeld = true;
-            stopLockedScreen();
+            removeLockedScreen();
         };
 
         Runnable run4 = () -> {
             lockedScreenModel.isButtonFourHeld = true;
-            stopLockedScreen();
+            removeLockedScreen();
         };
 
         lockedScreenModel.lockedScreenButtonOne.setOnTouchListener((v1, e1) ->
@@ -267,7 +329,7 @@ public class MainMenuService extends Service {
                 case (MotionEvent.ACTION_MOVE):
                     // ensure that the finger is not moved out of bounds
                     if(!lockedScreenModel.buttonTwoBounds.contains(v1.getLeft() + (int) e1.getX(), v1.getTop() + (int) e1.getY())){
-                        handler.removeCallbacks(run1);
+                        handler.removeCallbacks(run2);
                     }
                 default:
                     return false;
@@ -290,7 +352,7 @@ public class MainMenuService extends Service {
                 case (MotionEvent.ACTION_MOVE):
                     // ensure that the finger is not moved out of bounds
                     if(!lockedScreenModel.buttonThreeBounds.contains(v1.getLeft() + (int) e1.getX(), v1.getTop() + (int) e1.getY())){
-                        handler.removeCallbacks(run1);
+                        handler.removeCallbacks(run3);
                     }
                 default:
                     return false;
@@ -313,7 +375,7 @@ public class MainMenuService extends Service {
                 case (MotionEvent.ACTION_MOVE):
                     // ensure that the finger is not moved out of bounds
                     if(!lockedScreenModel.buttonFourBounds.contains(v1.getLeft() + (int) e1.getX(), v1.getTop() + (int) e1.getY())){
-                        handler.removeCallbacks(run1);
+                        handler.removeCallbacks(run4);
                     }
                 default:
                     return false;
@@ -325,14 +387,16 @@ public class MainMenuService extends Service {
 
     private void displayLockedScreen()
     {
+        // ensure locked screen is full screen
         lockedScreenModel.lockedScreen.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        // hides notification bar and nav bar if user swipes them
         lockedScreenModel.lockedScreen.setOnSystemUiVisibilityChangeListener(listener ->
                 lockedScreenModel.lockedScreen.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE)
         );
         this.windowManager.addView(lockedScreenModel.lockedScreen, lockedScreenModel.lockedScreenParams);
     }
 
-    private void stopLockedScreen()
+    private void removeLockedScreen()
     {
         if (lockedScreenModel.isButtonOneHeld)
         {
@@ -343,14 +407,20 @@ public class MainMenuService extends Service {
         }
 
     }
-
-    private void removeOptionsMenu()
-    {
-        this.windowManager.removeViewImmediate(optionsMenuModel.optionsMenu);
-    }
     //endregion
 
-//    // region Overriden Methods
+    // region Overriden Methods
+@Override
+public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+
+    // Checks the orientation of the screen
+    if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        this.currHeight = metrics.heightPixels;
+    } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+        this.currHeight = metrics.heightPixels;
+    }
+}
 //    @Override
 //    public void onDestroy()
 //    {
@@ -359,5 +429,5 @@ public class MainMenuService extends Service {
 //        }
 //        super.onDestroy();
 //    }
-//    // endregion
+    // endregion
 }
